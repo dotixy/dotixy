4f6b325ee6384572681af274bf12dfff16d9f403
ASonay <anilsonay@gmail.com>
2023-05-22 20:36:06 +0200

    Migrating web

--- webapp/homeapp/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-from django.db import models
-
-class Drawing(models.Model):
-    # a model of a drawing saved by the user
-    # store the drawing data as a base64 string
-    data = models.TextField()
