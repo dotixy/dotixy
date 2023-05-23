9f68156262023af84babe8eae58128db32088f9b
Mehmet Anil AKBAY <m.anil.akbay@gmail.com>
2023-05-23 23:13:35 +0200

    instance generator for routing

---
+++ dotixy/routing/instance_generator.py
@@ -0,0 +1,17 @@
+import numpy as np
+from typing import List
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class instance_generator:
+    def __init__ (
+            self,
+            ngeneration : int = 100
+    ):
+        self.ngeneration = ngeneration
+        logger.info('Generator initialized successfully..')
+
+    def generate(self) -> np.array:
+        return np.random.rand(self.ngeneration)
